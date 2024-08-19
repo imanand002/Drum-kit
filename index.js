0ec3c48942f77b35a58262fe00a3857ef97aee4f
imanand002 <mayank.gbps@gmail.com>
2024-08-19 21:45:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,68 @@
+//Playing sounds of instrument corresponding to the letter on the image using cursor.
+
+
+// var noOfDrums = document.querySelectorAll(".drum").length;
+// for (var i = 0; i < noOfDrums; i++) {
+//   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    
+//     var buttonInnerHTML = this.innerHTML;
+//     switch(buttonInnerHTML){
+//       case "w":
+//         var tom1 = new Audio('./sounds/tom-1.mp3');
+//         tom1.play();
+//         break;
+//       case "a":
+//         var tom2 = new Audio('./sounds/tom-2.mp3');
+//         tom2.play()
+//         break;
+//       case "s":
+//         var tom3 = new Audio('./sounds/tom-3.mp3');
+//         tom3.play();
+//         break;
+//       case "d":
+//         var tom4 = new Audio('./sounds/tom-4.mp3');
+//         tom4.play();
+//         break;
+//       case "j":
+//         var snare = new Audio('./sounds/snare.mp3');
+//         snare.play();
+//         break;
+//       case "k":
+//         var crash = new Audio('./sounds/crash.mp3');
+//         crash.play();
+//         break;
+//       case "l":
+//         var kick = new Audio('./sounds/kick-bass.mp3');
+//         kick.play();
+//         break;
+//       default: console.log(buttonInnerHTML);
+//     }
+//   });
+// }
+
+// ---------------------------------------------------------//
+
+//Playing sounds of instruments corresponding to the letter on the image using cursor as well as keyboard.
+
+//Detecting buttons press
+
 var noOfDrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i < noOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    //alert("I got clicked.");
+    
     var buttonInnerHTML = this.innerHTML;
-    switch(buttonInnerHTML){
+    makeSound(buttonInnerHTML);
+  });
+
+  //Detecting keyboard press
+
+  document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+  })
+}
+
+function makeSound(key){
+    switch(key){
       case "w":
         var tom1 = new Audio('./sounds/tom-1.mp3');
         tom1.play();
@@ -32,11 +91,7 @@ for (var i = 0; i < noOfDrums; i++) {
         var kick = new Audio('./sounds/kick-bass.mp3');
         kick.play();
         break;
-      default: console.log(buttonInnerHTML);
+      default: console.log(key);
     }
-  });
+
 }
-
-
-//var audio = new Audio('./sounds/tom-1.mp3');
- //   audio.play();
